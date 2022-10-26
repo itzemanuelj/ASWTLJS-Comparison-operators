31b91740e87b374c7da04994d99e97cc76eee7e6
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-25 23:15:29 -0400

    mvp commit for comparison operators

---
+++ script.js
@@ -0,0 +1,86 @@
+// ===, !==, >, and <= are ________ operators.
+// 
+// What is the comparison operator for is not equal to?
+if (a !== b) {
+
+}
+// What is the comparison operator for is greater than?
+if (a > a) {
+
+}
+// What is the comparison operator for is less than or equal to?
+if (a <= b) {
+
+}
+// Create this if statement: If a isn't equal to b, a is assigned the value of c.
+if (a !== b) {
+    a = c;
+}
+// Code the first line of an if statement that tests whether num1 is greater than num2.
+if (num1 >  num2) {
+
+}
+// Code the first line of an if statement that tests whether num1 is greater than or equal to num2.
+if (num1 >=  num2) {
+
+}
+// Code the first line of an if statement that tests whether one variable is unequal to another.
+if (cost !== time) {
+
+}
+// Code the first line of an if statement that tests whether a variable has a different value from a particular string.
+if (cost !== "cost") {
+
+}
+// Code the first line of an if statement that tests whether a variable is less than a particular number.
+if (time < 10) {
+
+}
+// Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+if (total >= cost) {
+
+}
+// Code the first line of an if statement that tests whether a string is unequal to the value represented by a particular variable.
+if ('time' !== time) {
+
+}
+// Code the first line of an if statement that tests whether one variable is unequal to another.
+if (cost !== time) {
+
+}
+// Code the first line of an if statement that tests whether a variable has a different value from a particular string.
+if (cost !== 'one hundred') {
+
+}
+// Code the first line of an if statement that tests whether a variable is less than a particular number.
+if (cost < 20) {
+
+}
+// Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+if (time >= cost) {
+
+}
+// Code the first line of an if statement that tests whether a string is unequal to the value represented by a particular variable.
+if ('100' !== oneHundred) {
+
+}
+// Code an if statement. Test whether a variable is unequal to a particular number. If so, assign a number to that variable.
+if (time !== 10) {
+    time = 9;
+}
+// Code an if statement that tests whether a number is unequal to a different number.
+// If the condition is true (it will be), display a congratulations alert.
+if (1 !== 2) {
+    alert('congratulations');
+}
+// Code a prompt asking for your first name.
+// Code an if statement that tests whether the name you entered is unequal to another name.
+// If the condition is true (it will be), display an alert that says "No match"
+var firstName = prompt('Enter First Name');
+
+if (firstName !== 'Justin') {
+   alert('No Match'); 
+}
+else {
+    alert('Correct name');
+}
